2a4acbea92cca477da9586f9de548e2afe4929b8
ala-hamadi <ala.hamadi.01@gmail.com>
2021-05-10 01:38:38 +0200

    Building Login page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
         />
         <div className='header_search'>
           <SearchIcon />
-          <input type='text' />
+          <input type='text' placeholder='Search' />
         </div>
       </div>
       <div className='header_right'>
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,31 @@
 import React from "react";
 import "./Login.css";
+import linkedin from "../../assets/images/linkedin.png";
 const Login = () => {
-  return <div className='login'></div>;
+  const handleRegister = () => {};
+  const handleLogin = () => {};
+
+  return (
+    <div className='login'>
+      <img src={linkedin} alt='' />
+
+      <form>
+        <input type='text' placeholder='Full name (required)' />
+        <input type='text' placeholder='Profile pic URL (optional)' />
+        <input type='Email' placeholder='Email (required)' />
+        <input type='Password' placeholder='Password (required)' />
+        <button type='submit' onClick={handleLogin}>
+          Sign In
+        </button>
+      </form>
+      <p>
+        Not a member?{" "}
+        <span className='login_register' onClick={handleRegister}>
+          Register Now
+        </span>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
